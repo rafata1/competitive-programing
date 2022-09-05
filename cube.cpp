128d879a13d21e5f7b654850c7a0f28d04387f7e
Cao Duc Anh Dung <dung.cda@teko.vn>
2022-09-05 11:43:45 +0700

    shit

--- cube.cpp
+++ cube.cpp
@@ -7,27 +7,27 @@ void printRes(int a, int b, int c, int d, int e, int f, int g) {
     return;
 }
 
-//int main() {
-//    int a, b, c;
-//    cin >> a >> b >> c;
-//
-//    if (a == 2 && b == 1 && c == 1) {
-//        printRes(1, 0, 0, 0, 0, 0, 1);
-//        return 1;
-//    }
-//
-//    if ((a == b && a == 2) || (a == c && a == 1) || (b == c && b == 1)) {
-//        printRes(1, 0, 0, 0, a * b * c - 2, 2, 0);
-//        return 1;
-//    }
-//
-//    if (min(a, min(b, c)) == 2) {
-//        int x = max(a, max(b,c));
-//        int y = a + b + c - x - 2;
-//        printRes(1, 0, (x - 2) * (y - 2), 2 * max(0, a - 2) + 2 * max(0, b - 2) + 2 * max(0, c - 2), 4, 0 , 0);
-//        return 1;
-//    }
-//
-//    printRes((a - 3) * (b - 2) * (c - 2), 2 * max(0, (a - 2) * (b - 2)) + 2 * max(0, (a - 2) * (c - 2)) + 2 * max(0, (b - 2) * (c - 2)), max(0, 4 * (a - 2)) + max(0, 4 * (b - 2)) + max(0, 4 * (c - 2)), 8 , 0 , 0, 0);
-//    return 1;
-//}
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == 1 && b == 1 && c == 1) {
+        printRes(0, 0, 0, 0, 0, 0, 1);
+        return 0;
+    }
+
+    if ((a == b && a == 1) || (a == c && a == 1) || (b == c && b == 1)) {
+        printRes(0, 0, 0, 0, a * b * c - 2, 2, 0);
+        return 0;
+    }
+
+    if (min(a, min(b, c)) == 1) {
+        int x = max(a, max(b,c));
+        int y = a + b + c - x - 1;
+        printRes(0, 0, (x - 2) * (y - 2), 2 * max(0, a - 2) + 2 * max(0, b - 2) + 2 * max(0, c - 2), 4, 0 , 0);
+        return 0;
+    }
+
+    printRes((a - 2) * (b - 2) * (c - 2), 2 * max(0, (a - 2) * (b - 2)) + 2 * max(0, (a - 2) * (c - 2)) + 2 * max(0, (b - 2) * (c - 2)), max(0, 4 * (a - 2)) + max(0, 4 * (b - 2)) + max(0, 4 * (c - 2)), 8 , 0 , 0, 0);
+    return 0;
+}
